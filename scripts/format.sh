5be0230b5d4bdb9627ca549c12544d959c114519
Raehyun Kang <rhkang.dev@gmail.com>
2025-02-19 23:26:23 +0900

    style: set clang-format

---
+++ scripts/format.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+script=$(readlink -f "$0")
+script_dir=$(dirname "$script")
+
+root=$(cd "$script_dir/../" && pwd)
+
+directories=(
+    "$root/src"
+    "$root/sandbox"
+)
+
+format_files() {
+    for dir in "${directories[@]}"; do
+        find "$dir" -name "*.cpp" -o -name "*.h" -o -name "*.hpp" | while read -r file; do
+            clang-format -i "$file"
+            echo "Formatted $file"
+        done
+    done
+}
+
+format_files
